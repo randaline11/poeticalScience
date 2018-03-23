6647e94ce2e92451e97ea2e616abeffc07077831
Abby Starr <astarr2015@gmail.com>
2018-03-22 23:16:45 -0400

    some more restful functions

--- app/controllers/book_controller.js
+++ app/controllers/book_controller.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 const Book = require('../models/book_model.js');
 
-export const createPost = (req, res) => {
-  console.log('request user:');
-  console.log(req.user.username);
-  const newPost = {
-    title: req.body.title ? req.body.title : 'undefined',
-    tags: req.body.tags ? req.body.tags : '',
-    cover_url: req.body.cover_url ? req.body.cover_url : '',
-    content: req.body.content ? req.body.content : '',
-    author: req.user.username,
+export const createBook = (req, res) => {
+  console.log('createBook');
+  const newBook = {
+    title: req.body.title ? req.body.title : undefined,
+    author: req.body.author ? req.body.author : '',
+    publisher: req.body.publisher ? req.body.publisher : '',
+    publish_place: req.body.publish_place ? req.body.publish_place : '',
+    id_goodreads: req.body.id_goodreads ? req.body.id_goodreads : undefined,
+    isbn: req.body.isbn ? req.body.isbn : undefined,
+    ratings: req.body.ratings ? req.body.ratings : 0,
+    reviews: req.body.reviews ? req.body.reviews : 0,
+    average_rating: req.body.average_rating ? req.body.average_rating : undefined,
+    first_publish_year: req.body.first_publish_year ? req.body.first_publish_year : undefined,
   };
-  console.log(`newPost authorName: ${newPost.author}`);
-  const post = new Post(newPost);
-  post.save()
+  const book = new Book(newBook);
+  book.save()
     .then((result) => {
       res.json(post);
     })
@@ -22,70 +25,83 @@ export const createPost = (req, res) => {
     });
 };
 
-const cleanPosts = (posts) => {
-  return posts.map((post) => {
-    console.log(`username? ${post.author}`);
-    return {
-      id: post._id, title: post.title, tags: post.tags, cover_url: post.cover_url, author: post.author,
-    };
-  });
-};
-
-export const getPosts = (req, res) => {
-  Post.find({})
+export const getBooks = (req, res) => {
+  console.log('getBooks');
+  Book.find({})
     .then((result) => {
-      const newClean = cleanPosts(result);
-      res.json(newClean);
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-};
-
-
-export const getPost = (req, res) => {
-  console.log('in getPost server');
-  Post.findById(req.params.someID)
-    .then((result) => {
+      console.log('result of getting book: ', result);
+      //  const newClean = cleanBooks(result);
       res.json(result);
     })
     .catch((error) => {
       res.status(500).json({ error });
     });
 };
-
-
-export const deletePost = (req, res) => {
-  console.log('in deletePost server');
-  Post.remove({ _id: req.params.someID })
-    .then((result) => {
-      console.log('successfully deleted the result');
-      res.json();
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-};
-
-
-export const updatePost = (req, res) => {
-  console.log('in updatePost server');
-
-  Post.findById(req.params.someID)
-    .then((post) => {
-      console.log('found an individual post updatePost');
-      post.title = req.body.title;
-      post.cover_url = req.body.cover_url;
-      post.content = req.body.content;
-      post.tags = req.body.tags;
-      post.save();
-      return (post);
-    })
-    .then((post1) => {
-      console.log('successfully updated the result');
-      res.json({ post: post1 });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-};
+//
+// const cleanPosts = (posts) => {
+//   return posts.map((post) => {
+//     console.log(`username? ${post.author}`);
+//     return {
+//       id: post._id, title: post.title, tags: post.tags, cover_url: post.cover_url, author: post.author,
+//     };
+//   });
+// };
+//
+// export const getPosts = (req, res) => {
+//   Post.find({})
+//     .then((result) => {
+//       const newClean = cleanPosts(result);
+//       res.json(newClean);
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error });
+//     });
+// };
+//
+//
+// export const getPost = (req, res) => {
+//   console.log('in getPost server');
+//   Post.findById(req.params.someID)
+//     .then((result) => {
+//       res.json(result);
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error });
+//     });
+// };
+//
+//
+// export const deletePost = (req, res) => {
+//   console.log('in deletePost server');
+//   Post.remove({ _id: req.params.someID })
+//     .then((result) => {
+//       console.log('successfully deleted the result');
+//       res.json();
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error });
+//     });
+// };
+//
+//
+// export const updatePost = (req, res) => {
+//   console.log('in updatePost server');
+//
+//   Post.findById(req.params.someID)
+//     .then((post) => {
+//       console.log('found an individual post updatePost');
+//       post.title = req.body.title;
+//       post.cover_url = req.body.cover_url;
+//       post.content = req.body.content;
+//       post.tags = req.body.tags;
+//       post.save();
+//       return (post);
+//     })
+//     .then((post1) => {
+//       console.log('successfully updated the result');
+//       res.json({ post: post1 });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error });
+//     });
+// };
--- app/controllers/weight_controller.js
+++ app/controllers/weight_controller.js
@@ -11,12 +11,45 @@ function createMultipleWeights(weights) {
   });
 }
 
-function findExactWeight(weight) {
-  Weight.find({ source: weight.source, target: weight.target })
+export const findWeight = (req, res) => {
+  Weight.find({ source: req.body.weight, target: req.body.target })
     .then((foundWeight) => {
       console.log('found this weight', foundWeight);
     })
     .catch((err) => {
 
     });
-}
+};
+
+export const getWeights = (req, res) => {
+  console.log('getWeights');
+  Weight.find({})
+    .then((result) => {
+      console.log('result of getting weight: ', result);
+      //  const newClean = cleanBooks(result);
+      res.json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
+export const createWeight = (req, res) => {
+  console.log('createWeight');
+  const newWeight = {
+    source: { type: String, default: '' },
+    target: { type: String, default: '' },
+    weight: { type: Number, default: 0 },
+    source: req.body.source ? req.body.source : '',
+    target: req.body.target ? req.body.target : '',
+    weight: req.body.weight ? req.body.weight : 0,
+  };
+  const weight = new Weight(newWeight);
+  weight.save()
+    .then((result) => {
+      res.json(post);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
